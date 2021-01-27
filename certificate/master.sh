8d6cebb6a1e4fd825e4b053d31ea5602691fd2ba
vijayrajan23 <vijayrajan23@gmail.com>
2021-01-27 21:53:06 +0530

    new config file

---
+++ certificate/master.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# master certificates move
+for instance in master1.mylap.in master2.mylap.in; do
+  scp ca.pem ca-key.pem kubernetes-key.pem kubernetes.pem service-account-key.pem service-account.pem \
+     admin.kubeconfig kube-controller-manager.kubeconfig kube-scheduler.kubeconfig	  ${instance}:~/
+done
